44081c2ee2311a542332742447c1b62583d29dac
Nsikak Itama <nsikak.itama@gmail.com>
2026-08-20 18:57:30 +0100

    testing

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -57,7 +57,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bcc131b3ddba913e23391a0cc1360f16
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "e5e5835a12439bb6dabccf9ce6d892df",
+"flutter_bootstrap.js": "7d361acc723ce93421690167dab0248f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
